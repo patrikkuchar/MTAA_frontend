4d06e615e4495e04d6f5a01d085cba58db2dff8a
Patrik Kuchár <patrik.kuchar171@gmail.com>
2022-04-06 21:14:00 +0200

    first commit - login

---
+++ app/src/main/java/com/example/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.content_main)
+        /*
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)*/
+    }
+}
